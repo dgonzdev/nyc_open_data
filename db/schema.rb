10dfccafc485df0295c8fa66a126eeb08353ef96
dgonzdev <dgonzdev837@gmail.com>
2025-11-13 20:46:02 -0500

    add index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_11_14_004802) do
+ActiveRecord::Schema[7.2].define(version: 2025_11_14_014438) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,5 +26,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_14_004802) do
     t.text "counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["original_id"], name: "index_bicycle_counters_on_original_id", unique: true
   end
 end
